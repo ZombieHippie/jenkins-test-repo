9ea1d32c26daa3f40c6dc29b956b3dbc28a39782
Cole R Lawrence <colelawr@gmail.com>
2016-06-12 22:23:06 -0500

    Create failing test

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -17,7 +17,7 @@ describe('App', () => {
   ]);
 
   it('should have a url', inject([ App ], (app) => {
-    expect(app.url).toEqual('https://twitter.com/Angular'); // should fail
+    expect(app.url).toEqual('https://twitter.com/Angular') // should fail
   }));
 
 });
